d5f18d090f56123cb5a7b0dd0f8a77fd0e07bb8a
U!7 <235654398+Linl777@users.noreply.github.com>
2026-04-16 13:56:08 +0200

    production ready version

--- src/kpi_calculator.py
+++ src/kpi_calculator.py
@@ -6,13 +6,13 @@ def calculate_kpis(df):
     total_revenue = df['revenue'].sum()
     total_volume = df['volume'].sum()
     avg_price = total_revenue / total_volume
-    price_erosion = df['last_year_price'] / df['price'] - 1
+    #price_erosion = df['last_year_price'] / df['price'] - 1
 
     kpis = {
         "Total Revenue": f"{total_revenue:,.0f}",
         "Total Volume": f"{total_volume:,.0f}",
-        "Average Price": f"{avg_price:.2f}",
-        "Price Development": f"{price_erosion:.2f}"
+        "Average Price": f"{avg_price:.2f}"
+        #"Price Development": f"{price_erosion:.2f}"
     }
 
 
