ede3dec49af405e5f7cae17a5d57591e2d7602e7
U!7 <235654398+Linl777@users.noreply.github.com>
2026-04-17 16:51:37 +0200

    production ready version

--- app.py
+++ app.py
@@ -239,15 +239,15 @@ with tab2:
                         
                 To copy-paste the question into chat field below to ask AI
                     
-                    - Give me a summary of Customers and Products Performance in 2026
-                    - For customer C, have we achieved our goal of year 2026
-                    - Highlight me the product(s) that with big price erosion issue.
-                    - What product should be focus in near term for European customers.
-                    - I'll have a re-negotiation with customer E soon to sell Prod D to the customer, give me some insight on prepping for the customer engagement meeting.
-                    - I'll have a business review with my leadership team(c-level), give me a summary on what's happening and risk and opportunities.
-                    - I have a new product Prod F a next gen product of Prod A to sell to the customer A soon, for a successful price positioning, give me some insights to prep for this.
+                - Give me a summary of Customers and Products Performance in 2026
+                - For customer C, have we achieved our goal of year 2026
+                - Highlight me the product(s) that with big price erosion issue.
+                - What product should be focus in near term for European customers.
+                - I'll have a re-negotiation with customer E soon to sell Prod D to the customer, give me some insight on prepping for the customer engagement meeting.
+                - I'll have a business review with my leadership team(c-level), give me a summary on what's happening and risk and opportunities.
+                - I have a new product Prod F a next gen product of Prod A to sell to the customer A soon, for a successful price positioning, give me some insights to prep for this.
 
-                Or to ask any qustions in the text field below 
+                Or to type in any qustions in the text field below.
             """)
 
     # ------------------------
